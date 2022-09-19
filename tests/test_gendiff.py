f172f44458346f4b3d837ca4185d2eb7cbb95b8b
Aleksei Seleznev <ferfa2@rambler.ru>
2022-09-19 21:30:35 +0300

    add tests for nested structures

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -60,3 +60,36 @@ def test_generate_diff3(file1_yml, file2_yml):
 def test_generate_diff4(file3_yaml, file4_yaml):
     correct_result = open('tests/fixtures/correct_result_files_3_4.txt')
     assert generate_diff(file3_yaml, file4_yaml) == correct_result.read()
+
+
+@pytest.fixture
+def file1_nested_json():
+    return 'tests/fixtures/file1_nested.json'
+
+
+@pytest.fixture
+def file2_nested_json():
+    return 'tests/fixtures/file2_nested.json'
+
+
+def test_generate_nested_diff1(file1_nested_json, file2_nested_json):
+    correct_result = open('tests/fixtures/correct_result_files_1_2_nested.txt')
+    assert generate_diff(file1_nested_json, file2_nested_json) == correct_result.read()
+
+
+@pytest.fixture
+def file1_nested_yaml():
+    return 'tests/fixtures/file1_nested.yaml'
+
+
+@pytest.fixture
+def file2_nested_yaml():
+    return 'tests/fixtures/file2_nested.yaml'
+
+
+def test_generate_nested_diff2(file1_nested_yaml, file2_nested_yaml):
+    correct_result = open('tests/fixtures/correct_result_files_1_2_nested.txt')
+    assert generate_diff(file1_nested_yaml, file2_nested_yaml) == correct_result.read()
+
+
+
